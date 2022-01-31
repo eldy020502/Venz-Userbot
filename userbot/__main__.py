99781309cb1c2ce3af6f563a022edf28956352a4
eldy020502 <96163277+eldy020502@users.noreply.github.com>
2022-01-31 12:13:32 +0800

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 # bot.loop.run_until_complete(checking())
 LOGS.info(
-    f"✨Kyy-Userbot✨ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"👉👌Venz-Userbot👉👌 ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
