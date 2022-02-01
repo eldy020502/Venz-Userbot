62913f650c8518d7d38f7ece45a00f08bbde18fe
eldy020502 <96163277+eldy020502@users.noreply.github.com>
2022-02-01 22:14:26 +0800

    Update kyyhelper.py

--- userbot/modules/kyyhelper.py
+++ userbot/modules/kyyhelper.py
@@ -11,17 +11,17 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern="^.lhelp$")
 async def usit(e):
     await e.edit(
-        f"**Halo {DEFAULTUSER} Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        "\n[Telegram](t.me/IDnyaKosong)"
-        "\n[Repo](https://github.com/S/Kyy-Userbot)"
-        "\n[Instagram](instagram.com/rizkyhamdanii16_)")
+        f"**Halo {DEFAULTUSER} Kalo Lu Ga Tau Cara Gunain Bot Gw Lu Bisa Ketik** `.help` Atau Lu Juga Bisa Minta Bantuan Ke:\n"
+        "\n[Telegram](t.me/vnotv)"
+        "\n[Repo](https://github.com/S/Venz-Userbot)"
+        "\n[Instagram](instagram.com/eldydwinggga_)")
 
 
 @register(outgoing=True, pattern="^.vars$")
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/muhammadrizky16/Kyy-Userbot/Kyy-Userbot/varshelper.txt)")
+        "\n[DAFTAR VARS](https://raw.githubusercontent.com/eldy020502/Venz-Userbot/Venz-Userbot/varshelper.txt)")
 
 
 CMD_HELP.update({
