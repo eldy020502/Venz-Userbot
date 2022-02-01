75ac6a1692b610948394b779cea6906fbdf4af52
eldy020502 <96163277+eldy020502@users.noreply.github.com>
2022-02-02 01:35:48 +0800

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -33,9 +33,9 @@ async def help(event):
         for i in CMD_HELP:
             string += "`" + str(i)
             string += "`\t ✗  "
-        await event.edit("**✨ҡʏʏ-υѕєявσт✨**\n\n"
+        await event.edit("**👉👌Venz-UserBot 👉👌**\n\n"
                          f"**◉ Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**◉ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
                          "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
-                         f"◉ {string}◉\n\n✐ **ɴᴏᴛᴇꜱ :**  `.help animasi`\n☞  sᴜᴘᴘᴏʀᴛ : @NastySupportt")
+                         f"◉ {string}◉\n\n✐ **ɴᴏᴛᴇꜱ :**  `.help animasi`\n☞  sᴜᴘᴘᴏʀᴛ : @justvenzzz")
         await asyncio.sleep(1000)
         await event.delete()
