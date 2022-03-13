3de9ecb8eac1265b1a5862469140d367a64ba346
eldy020502 <96163277+eldy020502@users.noreply.github.com>
2022-03-13 15:29:11 +0800

    Add files via upload

---
+++ userbot/modules/asupan.py
@@ -0,0 +1,40 @@
+# 🍀 © @tofik_dn
+# ⚠️ Do not remove credits
+
+
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP
+from userbot.utils import kyy_cmd
+import random
+from userbot import owner
+from telethon.tl.types import InputMessagesFilterVideo
+
+
+@kyy_cmd(pattern="asupan$")
+async def _(event):
+    try:
+        asupannya = [
+            asupan
+            async for asupan in event.client.iter_messages(
+                "@AsupanKyyUserbot", filter=InputMessagesFilterVideo
+            )
+        ]
+        aing = await event.client.get_me()
+        await event.client.send_file(
+            event.chat_id,
+            file=random.choice(asupannya),
+            caption=f"ᴀsᴜᴘᴀɴ ʙʏ [{owner}](tg://user?id={aing.id})",
+        )
+        await event.delete()
+    except Exception:
+        await event.edit("Tidak bisa menemukan video asupan.")
+
+
+CMD_HELP.update(
+    {
+        "asupan": f"**Plugin : **`asupan`\
+        \n\n  •  **Syntax :** `{cmd}asupan`\
+        \n  •  **Function : **Untuk Mengirim video asupan secara random.\
+    "
+    }
+)
