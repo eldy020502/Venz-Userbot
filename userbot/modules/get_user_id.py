01b73084c1b61c1c2774469bf1bc2fc07ee957c3
eldy020502 <96163277+eldy020502@users.noreply.github.com>
2022-04-11 00:36:59 +0700

    Add files via upload

--- userbot/modules/get_user_id.py
+++ userbot/modules/get_user_id.py
@@ -1,10 +1,10 @@
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot.utils import edit_or_reply, edit_delete, kyy_cmd
+from userbot.utils import edit_or_reply, edit_delete, venz_cmd
 from userbot import bot, CMD_HELP, CMD_HANDLER as cmd
 
 
-@kyy_cmd(pattern="getid(?: |$)(.*)")
+@venz_cmd(pattern="getid(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
