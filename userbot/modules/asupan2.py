ce16456506cd118907b1a6013af78df9916cbf99
eldy020502 <eldydwinggasaputra02@gmail.com>
2022-04-22 13:46:18 +0000

    pylint: auto fixes
    Signed-off-by: eldy020502 <eldydwinggasaputra02@gmail.com>

--- userbot/modules/asupan2.py
+++ userbot/modules/asupan2.py
@@ -55,7 +55,7 @@ CMD_HELP.update({
     "asupan2": f"𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `{cmd}pedo`\
     \n↳ : untuk mendapatkan video pedo random`\
     \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `{cmd}onlyfans`\
-    \n↳: Untuk Mendapatkan Video Onlyfans Random.
+n↳: Untuk Mendapatkan Video Onlyfans Random.
 "
-    }
+}
 )
